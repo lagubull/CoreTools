f8ebb0645796b8f66ca2cb203316abf431d961f1
Javier Laguna <lagubull@hotmail.com>
2016-01-11 12:42:34 +0000

    Initial Commit

---
+++ CoreTools/CoreTools.h
@@ -0,0 +1,19 @@
+//
+//  CoreTools.h
+//  CoreTools
+//
+//  Created by Javier Laguna on 11/01/2016.
+//  Copyright © 2016 Javier Laguna. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CoreTools.
+FOUNDATION_EXPORT double CoreToolsVersionNumber;
+
+//! Project version string for CoreTools.
+FOUNDATION_EXPORT const unsigned char CoreToolsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CoreTools/PublicHeader.h>
+
+
